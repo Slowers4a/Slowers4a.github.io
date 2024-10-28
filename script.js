8effe3e765e6742f0b4a526ca8ab3048619a3a86
SlowersMD <slowers4a@gmail.com>
2024-10-28 07:17:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 
-var display = 0;
-
-function lol() {
-  if (display == 0) {
-   document.getElementById('hasil').innerHTML = "Hallo"
-   display = 1;
-  } else {
-     document.getElementById('hasil').innerHTML = ""
-     display = 0;
-   }
-  
-}
+function toggleContent() {
+      const content = document.getElementById('content');
+      content.classList.toggle('show');
+    }
